f7c78c53e921d0abe5d68e29d676f3b41b451696
Nanyou <nanyouguan@gmail.com>
2015-10-13 17:49:41 -0400

    added weapon switch

--- source/weapon/Sniper.hx
+++ source/weapon/Sniper.hx
@@ -18,7 +18,7 @@ class Sniper extends Gun
 	public function new(state:PlayState)//, damageMask:Int, cooldownPerShot:Float, bulletRadius:Float, bulletLifespan:Float, bulletSpeed:Float, bulletColor:Int) 
 	{
 		state.add(this);
-		super(state, DamageMask.POD, reloadTime, bulletSize, lifespan, speed, amount, color);
+		super(state, DamageMask.PLAYER, reloadTime, bulletSize, lifespan, speed, amount, color);
 		
 	}
 	
