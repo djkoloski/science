31dd7f5f7afc25b1ff4b33ab413446a5fa6bc694
Sam Kasura <kasurs@rpi.edi>
2015-10-14 02:46:56 -0400

    weapons balanced

--- source/weapon/StartingGun.hx
+++ source/weapon/StartingGun.hx
@@ -11,10 +11,10 @@ class StartingGun extends Gun
 	//public var cooldownTimer:Float;
 	public var reloadTime:Float = .5;
 	public var bulletSize:Float = 4;
-	public var lifespan:Float = .2;
+	public var lifespan:Float = .3;
 	public var speed:Float = 900;
 	public var color:Int = FlxColor.RED;
-	public var amount:Int = 10;
+	public var amount:Int = 15;
 	public function new(state:PlayState)//, damageMask:Int, cooldownPerShot:Float, bulletRadius:Float, bulletLifespan:Float, bulletSpeed:Float, bulletColor:Int) 
 	{
 		state.add(this);
